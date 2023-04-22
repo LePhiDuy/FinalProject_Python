431c9f6ca1477cc4e8060104f8c453fccee389ce
vanfulong <vanfulong@gmail.com>
2023-04-22 23:17:03 +0700

    add login + register page

---
+++ coffee-shop/coffee/forms.py
@@ -0,0 +1,6 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+class createuserform(UserCreationForm):
+    class Meta:
+        model=User
+        fields=['username','password1','password2', 'email']
