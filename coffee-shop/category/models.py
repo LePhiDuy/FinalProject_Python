c780cc226498197af370b97fd9f5da730b4c106b
LePhiDuy <duyphi2011@gmail.com>
2023-05-06 21:00:37 +0700

    up

--- coffee-shop/category/models.py
+++ coffee-shop/category/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 
 # Create your models here.
 class Category(models.Model):
@@ -7,6 +7,9 @@ class Category(models.Model):
     slug = models.SlugField(max_length=100, unique=True)
     image = models.ImageField(max_length=100, upload_to='categories/images', blank=True)
     description = models.CharField(max_length=2048, blank=True)
+
+    def get_url(self):
+        return reverse('products_by_category', args=[self.slug])
 
     def __str__(self):
         return self.name
